91819fd8f9ba6502d1de5aa16c5c0586e26cba4c
robin nganga smith <saintofcookies.2021@gmail.com>
2023-10-15 16:15:22 +0300

    connected html elements into jsfile into variables

---
+++ script.js
@@ -0,0 +1,7 @@
+const accessKey = '3FA5NpfDVoOKPcZs5Sq4FySILoiD1QLPZ20zurby8gk';
+
+const searchForm = document.getElementById('search-form');
+const searchBox = document.getElementById('search-box');
+const searchResult = document.getElementById('search-result');
+const showMoreBtn = document.getElementById('show-more-btn');
+const refreshBtn = document.getElementById('refresh');
